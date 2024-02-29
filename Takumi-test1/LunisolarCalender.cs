fc2d5a054099294a9c571cd52a75965c52ddf602
dammy216 <dammy3612@icloud.com>
2024-02-29 23:53:41 +0900

    プロジェクト ファイルを追加します。

---
+++ Takumi-test1/LunisolarCalender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakumiTest1
+{
+    class LunisolarCalender
+    {
+        protected int year = 0;
+        protected int month = 0;
+        protected int day = 0;
+
+        public LunisolarCalender(int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        protected int getLunarCalender()
+        {
+        JapaneseLunisolarCalendar jpnLunarlCal = new JapaneseLunisolarCalendar();
+        int lunarMonth = jpnLunarlCal.GetMonth
+        }
+
+    }
+}
---
+++ Takumi-test1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TakumiTest1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("年を入力してください");
+        int year = int.Parse(Console.ReadLine());
+        if (year < 1) return;
+
+        Console.WriteLine("月を入力してください");
+        int month = int.Parse(Console.ReadLine());
+        if (month < 1 || month > 12) return;
+
+        Console.WriteLine("日を入力してください");
+        int day = int.Parse(Console.ReadLine());
+        if (day < 1 || day > DateTime.DaysInMonth(year, month)) return;
+
+        var lunarCalender = new LunisolarCalender(year, month, day);
+        
+
+        Console.WriteLine($"{year}年{month}月{day}日はです");
+    }
+}
