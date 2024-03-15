8048839abf71bbf8ca49e08eba891dcaf7c7f7a0
dammy216 <dammy3612@icloud.com>
2024-03-15 13:26:09 +0900

    LunisolarCalenderクラスの削除

--- Takumi-test1/Rokuyo.cs
+++ Takumi-test1/Rokuyo.cs
@@ -1,10 +1,20 @@
-﻿namespace TakumiTest1
+﻿using System.Globalization;
+
+namespace TakumiTest1
 {
-    internal class Rokuyo : LunisolarCalender
+    internal class Rokuyo
     {
+        //年月日の初期化
+        private int year = 0;
+        private int month = 0;
+        private int day = 0;
+
         //コンストラクタ
-        public Rokuyo(int year, int month, int day) : base(year, month, day)
+        public Rokuyo(int year, int month, int day)
         {
+            this.year = year;
+            this.month = month;
+            this.day = day;
         }
 
         //六曜の一覧
@@ -16,6 +26,34 @@
             友引,
             先負,
             仏滅
+        }
+
+        /// <summary>
+        /// 旧暦の取得とうるう月の計算をする関数
+        /// </summary>
+        /// <returns>うるう月補正後の旧歴の月と旧暦の日</returns>
+        private (int, int) GetLunisolarCalender()
+        {
+            var jpnLunisolarlCal = new JapaneseLunisolarCalendar();     //旧暦を取得するクラス
+            var dataTime = new DateTime(year, month, day);              //入力された年月日を持つ変数
+
+            //月と月ごとの日にちを取得
+            int lunisolarMonth = jpnLunisolarlCal.GetMonth(dataTime);       //旧暦の月
+            int lunisolarDay = jpnLunisolarlCal.GetDayOfMonth(dataTime);    //旧暦の日
+
+            //うるう月の計算
+            int leapMonth = jpnLunisolarlCal.GetLeapMonth(      //うるう月をを持つ変数
+                jpnLunisolarlCal.GetYear(dataTime),
+                jpnLunisolarlCal.GetEra(dataTime)
+                );
+
+            //閏月の補正
+            if ((leapMonth > 0) && (lunisolarMonth - leapMonth >= 0))
+            {
+                lunisolarMonth = lunisolarMonth - 1;              //旧暦月を補正
+            }
+
+            return (lunisolarMonth, lunisolarDay);
         }
 
         /// <summary>
