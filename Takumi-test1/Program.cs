571f73f2dfefcd99b269d207e8d749b15da90abb
dammy216 <dammy3612@icloud.com>
2024-03-15 13:05:49 +0900

    parseをtryparseに修正

--- Takumi-test1/Program.cs
+++ Takumi-test1/Program.cs
@@ -6,7 +6,7 @@ internal class Program
     {
         //年の入力
         Console.WriteLine("年を入力してください");
-        int year = int.Parse(Console.ReadLine());
+        int.TryParse(Console.ReadLine(), out int year); //tryparseで変換可能か確認する
         if (year < 1960 || year > 2050)
         {
             Console.WriteLine("1960年から2050年までを入力してください");
@@ -15,7 +15,7 @@ internal class Program
 
         //月の入力
         Console.WriteLine("月を入力してください");
-        int month = int.Parse(Console.ReadLine());
+        int.TryParse(Console.ReadLine(), out int month);
         if (month < 1 || month > 12)
         {
             Console.WriteLine("1月から12月までを入力してください");
@@ -24,13 +24,13 @@ internal class Program
 
         //日の入力
         Console.WriteLine("日を入力してください");
-        int day = int.Parse(Console.ReadLine());
+        int.TryParse(Console.ReadLine(), out int day);
         if (day < 1 || day > DateTime.DaysInMonth(year, month))
         {
             Console.WriteLine("月の規定日内で入力してください");
             return;
         }
-        if(year == 1960 && day < 28 || year == 2050 && day > 22)
+        if (year == 1960 && day < 28 || year == 2050 && day > 22)
         {
             Console.WriteLine("1960年28日以降、2050年22日以内で入力してください");    //jpnLunisolarcalがこの日付までしか対応していないため
             return;
